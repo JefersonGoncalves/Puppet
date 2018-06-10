0cf17e45237a62e7a29dcac91055206137a94cee
root <root@master.puppet.vm>
2018-06-10 17:16:28 -0400

    Adding a valid key

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -9,6 +9,6 @@ class profile::base {
  		ensure => present,
 		user   => 'jgoncalves',
 		type   => 'ssh-rsa',
-		key    => 'This is a test',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCspd2cy0JOlZwH09M4fYOQjBN0OOFzDavtFLBfBpm1QPzoEto+b65mK7OFIcU7ryC6tfrVr0F8z1UD2mk3fEJWIGlD0KUYXhFx2WlbvuJl7D8IxRmJwWw5wiGNHbWPXCU1aktUBQXylpv452Wm1g2DORRrBOVWDbuv/SbfsgnU4K/2S5QQMI6Zx9QC6ZxaQKadAnzwF36LCb3TWZxRwINkIug26ELhjY7IVh87zEC8it1mb2CkWoP43mbvVWZG5FLVOUrk5ds7C059wubF239Bn/USV53WLKc2CLoRfDRj/yBakM6xi0qBaANnApRjopAfW/SuzBIWqEaeVe3TR6lF',
 	}
 }
